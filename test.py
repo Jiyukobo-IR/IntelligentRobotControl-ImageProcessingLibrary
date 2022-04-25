ccf2902cc4b82ec0d08594141213b41d8dc51256
kaito-IR <78997423+kaito-IR@users.noreply.github.com>
2022-04-25 11:25:27 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,19 @@
 import cv2
 import ColorBallSearch
+import serial
+import LineTrace
 ball = ColorBallSearch.ColorBallSearch()
+trace = LineTrace.LineTrace()
 cap = cv2.VideoCapture(0)
 FindColor = "None"
 while True:
     _,frame = cap.read()
     color_frame = cv2.resize(frame,dsize=(640,480))
+    gray_frame = cv2.cvtColor(color_frame,cv2.COLOR_BGR2GRAY)   
+    L_PWM,R_PWM,Slip = trace.LineTrace(gray_frame)
     cor,color_frame,x,y = ball.SearchColorBall(color_frame,FindColor)
     FindColor = cor
-    print(FindColor)    
     cv2.imshow("",color_frame)
     if cv2.waitKey(1) != -1:
+        ser.write(str.encode("End_\r\n"))
         break
