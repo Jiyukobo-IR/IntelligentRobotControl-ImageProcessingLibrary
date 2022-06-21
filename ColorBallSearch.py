a5da132527dcf0e546660e84d7e2bbe89b8f757d
Jiyukobo-IR <102468668+Jiyukobo-IR@users.noreply.github.com>
2022-06-21 16:50:28 +0900

    Add files via upload

--- ColorBallSearch.py
+++ ColorBallSearch.py
@@ -6,12 +6,12 @@ class ColorBallSearch:#画像処理によるボールの認識クラス
         #ボールキャッチエリアの座標指定
         #ボールキャッチエリアの四角の中にボールの中心座標が入ればそのボールは掴めると判断する
         self.x1 = 390#ボールキャッチエリアのx座標
-        self.x2 = 410
-        self.y1 = 285#ボールキャッチエリアのy座標
-        self.y2 = 320
+        self.x2 = 409
+        self.y1 = 272#ボールキャッチエリアのy座標
+        self.y2 = 307
         
         self.pos_old = [0,0,0]#認識したボール(ロックオンしたボール)の座標と半径
-        self.trimming_y = 150 #カメラ映像の上部を切り取り、見える範囲を調整する。
+        self.trimming_y = 110 #カメラ映像の上部を切り取り、見える範囲を調整する。
      
     def HoughBallScan(self,img):#ハフ変換によるボールの検出
         img_blur = cv2.GaussianBlur(img,(9,9),0)#ブラー処理をかけボールの輪郭をぼかす
@@ -21,7 +21,7 @@ class ColorBallSearch:#画像処理によるボールの認識クラス
         gray = cv2.cvtColor(img_adjust,cv2.COLOR_BGR2GRAY)#画像をグレースケール化
         
         #ハフ変換によるボールの検出を行う。HoughCirclesの詳しい情報は自分で調べること
-        circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,dp=1.3,minDist=40,param1=100,param2=40,maxRadius=110)
+        circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,dp=1.3,minDist=30,param1=100,param2=40,maxRadius=110)
         if circles is not None:#ボールが検出できればそれをuint16にキャストする
             circles = np.uint16(np.around(circles))
         return circles
@@ -38,9 +38,9 @@ class ColorBallSearch:#画像処理によるボールの認識クラス
         #各色の領域を閾値でフィルタリング
         red_min = np.array([0,120,100]) #色相(Hue)、彩度(Saturation)、明度(Value)
         red_max = np.array([2,255,255])
-        blue_min = np.array([90,230,70])
-        blue_max = np.array([120,255,150])
-        yellow_min = np.array([20,160,90])
+        blue_min = np.array([90,220,0])
+        blue_max = np.array([120,255,255])
+        yellow_min = np.array([20,160,70])
         yellow_max = np.array([30,210,140])
         mask = []
         if SearchColor == "Yellow":
@@ -95,9 +95,8 @@ class ColorBallSearch:#画像処理によるボールの認識クラス
                 #領域のカタマリである「ブロブ」を識別し、データを格納する。すごくありがたい機能。
                 nLabels, labelimages, data, center = cv2.connectedComponentsWithStats(mask)
                 blob_count = nLabels - 1 #ブロブの数。画面領域全体を1つのブロブとしてカウントするので、-1する。
-                
                 #前回認識したボールと同一のボールかを確認する。これをしないと同じ色のボールが画像内に映り込んでいる場合にそちらを追いかけようとしてしまう
-                if blob_count >= 1 and circle[0]+circle[2] > self.pos_old[0]-self.pos_old[2]-30 and circle[0]-circle[2] < self.pos_old[0]+self.pos_old[2]+30 and circle[1]+circle[2] > self.pos_old[1]-self.pos_old[2]-30 and circle[1]-circle[2] < self.pos_old[1]+self.pos_old[2]+30:
+                if blob_count >= 1 and circle[0]+circle[2] > self.pos_old[0]-self.pos_old[2]-50 and circle[0]-circle[2] < self.pos_old[0]+self.pos_old[2]+50 and circle[1]+circle[2] > self.pos_old[1]-self.pos_old[2]-50 and circle[1]-circle[2] < self.pos_old[1]+self.pos_old[2]+50:
                     self.pos_old = circle
                     cv2.circle(circleimg,(circle[0],circle[1]),circle[2],(40,165,255),1)
                     cv2.circle(circleimg,(circle[0],circle[1]),2,(0,255,0),1)
@@ -107,3 +106,88 @@ class ColorBallSearch:#画像処理によるボールの認識クラス
         #ループを抜け切った場合見つからなかった扱いにする
         Find = "None"
         return Find,frame,0,0
+    
+    def CatchConfirmation(self,frame,findcolor):#ボールの検出とそれに応じた処理を行う。findcolorの中にはロックオンしているボールの色を指定する
+        Color = ["Blue","Red","Yellow"]
+        Find = "None"
+        #cv2.imshow("beforetrim",frame)
+        #cv2.imshow("aftertrim",frame)
+        circles = self.HoughBallScan(frame)
+        if circles is None:#ボールが見つからなかった場合はここに入る
+            Find = "None"
+            return Find,frame,0,0
+        Balls = []
+        for circle in circles[0,:]:#circlesの各要素を抽出しfor文を回す
+            circleimg = frame.copy()
+            h,w = circleimg.shape[:2]
+            Houghmask = np.zeros((h,w),dtype=np.uint8)#画像を真っ黒に上塗りする
+            cv2.circle(Houghmask,(circle[0],circle[1]),circle[2],255,-1)#型抜きの要領で黒塗りの画像からボールの部分だけを抽出
+            circleimg[Houghmask==0] = [0,0,0]
+            circleimg = np.array(circleimg)
+            for cor in Color:
+                mask = self.red_range(circleimg,cor) #frameデータをnp配列に変換。
+                if len(mask) == 0:
+                    continue
+                #領域のカタマリである「ブロブ」を識別し、データを格納する。すごくありがたい機能。
+                nLabels, labelimages, data, center = cv2.connectedComponentsWithStats(mask)
+                blob_count = nLabels - 1 #ブロブの数。画面領域全体を1つのブロブとしてカウントするので、-1する。
+                if blob_count >= 1:
+                    cv2.circle(circleimg,(circle[0],circle[1]),circle[2],(40,165,255),1)#認識したボールをマーキング
+                    cv2.circle(circleimg,(circle[0],circle[1]),2,(0,255,0),1)
+                    Find = cor
+                    cv2.rectangle(circleimg,(self.x1,self.y2),(self.x2,self.y1),(0,255,0),2)#ボールのキャッチエリアを可視化する。この四角の中にボールの中心座標が入ればそのボールは掴めると判断する
+                    Balls.append([Find,circleimg,circle[0],circle[1]])#認識したボールの色,画像データ,認識したボールのx座標,y座標
+        i = 0
+        for ball in Balls:
+            if ball[0] == findcolor and ball[2] > self.x1 and ball[2] < self.x2 and ball[3] > self.y1 and ball[3] < self.y2:
+                Balls.pop(i)
+                for otherball in Balls:
+                    if otherball[2] > self.x1-80 and otherball[2] < self.x2+80 and otherball[3] > self.y2+10:
+                        return False
+                return True
+            i = i+1
+        return False    
+    
+    def CatchConfirmation_Vertical(self,frame,findcolor):#ボールの検出とそれに応じた処理を行う。findcolorの中にはロックオンしているボールの色を指定する
+        Color = ["Blue","Red","Yellow"]
+        Find = "None"
+        #cv2.imshow("beforetrim",frame)
+        #cv2.imshow("aftertrim",frame)
+        circles = self.HoughBallScan(frame)
+        if circles is None:#ボールが見つからなかった場合はここに入る
+            Find = "None"
+            return Find,frame,0,0
+        Balls = []
+        for circle in circles[0,:]:#circlesの各要素を抽出しfor文を回す
+            circleimg = frame.copy()
+            h,w = circleimg.shape[:2]
+            Houghmask = np.zeros((h,w),dtype=np.uint8)#画像を真っ黒に上塗りする
+            cv2.circle(Houghmask,(circle[0],circle[1]),circle[2],255,-1)#型抜きの要領で黒塗りの画像からボールの部分だけを抽出
+            circleimg[Houghmask==0] = [0,0,0]
+            circleimg = np.array(circleimg)
+            for cor in Color:
+                mask = self.red_range(circleimg,cor) #frameデータをnp配列に変換。
+                if len(mask) == 0:
+                    continue
+                #領域のカタマリである「ブロブ」を識別し、データを格納する。すごくありがたい機能。
+                nLabels, labelimages, data, center = cv2.connectedComponentsWithStats(mask)
+                blob_count = nLabels - 1 #ブロブの数。画面領域全体を1つのブロブとしてカウントするので、-1する。
+                if blob_count >= 1:
+                    cv2.circle(circleimg,(circle[0],circle[1]),circle[2],(40,165,255),1)#認識したボールをマーキング
+                    cv2.circle(circleimg,(circle[0],circle[1]),2,(0,255,0),1)
+                    Find = cor
+                    cv2.rectangle(circleimg,(self.x1,self.y2),(self.x2,self.y1),(0,255,0),2)#ボールのキャッチエリアを可視化する。この四角の中にボールの中心座標が入ればそのボールは掴めると判断する
+                    Balls.append([Find,circleimg,circle[0],circle[1],circle[2]])#認識したボールの色,画像データ,認識したボールのx座標,y座標
+        i = 0
+        for ball in Balls:
+            if ball[0] == findcolor and ball[2] > self.x1-50 and ball[2] < self.x2+50:
+                self.pos_old = [ball[2],ball[3],ball[4]]
+                Balls.pop(i)
+                for otherball in Balls:
+                    if otherball[2] > self.x1-20 and otherball[2] < self.x2+20 and otherball[3] > ball[3]:
+                        return False,otherball[0],otherball[2],otherball[3]
+                return True,ball[0],ball[2],ball[3]
+            i = i+1
+        return False,findcolor,self.pos_old[0],self.pos_old[1]
+                    
+            
--- LineTrace.py
+++ LineTrace.py
@@ -1,22 +1,23 @@
 import cv2
+import numpy as np
 
 class LineTrace:
     def __init__(self): #クラスが呼び出されるとまずここが呼び出され初期値の設定等を行う
-        self.trim_y = 180 #カメラから取得した画像を切り取る際のy座標
+        self.trim_y = 165 #カメラから取得した画像を切り取る際のy座標
         self.trim_h = 45 #カメラから取得した映像を切り取る際の切り取る縦の範囲
         self.th = 40 #黒い線と白い床面を見分けるための閾値
         self.i_max = 255 #閾値を超えた画素は全てこの値(白)として扱われる
-        self.LB_x1 = 155 #十字認識エリアのx座標
-        self.LB_x2 = 170 
+        self.LB_x1 = 145 #十字認識エリアのx座標
+        self.LB_x2 = 155 
         self.LB_y1 = 0 #十字認識エリアのy座標
         self.LB_y2 = self.trim_h 
-        self.RB_x1 = 210 
-        self.RB_x2 = 225 
+        self.RB_x1 = 225 
+        self.RB_x2 = 235
         self.RB_y1 = 0 
         self.RB_y2 = self.trim_h 
-        self.Kp = 1.220 #PID制御を行うためのPゲイン
-        self.Ki = 0.025 #PID制御を行うためのIゲイン
-        self.Kd = 0.700 #PID制御を行うためのDゲイン
+        self.Kp = 1.250 #PID制御を行うためのPゲイン
+        self.Ki = 0.005 #PID制御を行うためのIゲイン
+        self.Kd = 0.600 #PID制御を行うためのDゲイン
         self.L_PWM_old = 0 #ひとつ前のPWMの値
         self.R_PWM_old = 0 
         self.L_e1 = 0 #前回の制御時の偏差
@@ -28,11 +29,21 @@ class LineTrace:
         self.L_e = 0 #偏差
         self.R_e = 0 
         self.Goal = 190 #目標値
-
+        self.cx_old = self.Goal
+        
+    def adjust(self,img, alpha=1.0, beta=0.0):#画像のコントラストと明度に補正をかける(alpha:0.0~2.0,beta:-100.0~100.0の範囲をとる)
+        # 積和演算を行う。
+        dst = alpha * img + beta
+        # [0, 255] でクリップし、uint8 型にする。
+        return np.clip(dst, 0, 255).astype(np.uint8)
+    
     def LineTrace(self,frame):
         Slip = "None"
         frame = cv2.resize(frame,dsize=None,fx=0.5,fy=0.5)#処理の高速化のため画像のサイズを縮小
-        ret,frame = cv2.threshold(frame,self.th,self.i_max,cv2.THRESH_BINARY_INV)#画像を二値化(白黒の画像に変換する様なもの)
+        #ret,frame = cv2.threshold(frame,self.th,self.i_max,cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)#画像を二値化(白黒の画像に変換する様なもの)
+        frame = cv2.adaptiveThreshold(frame,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,51,20)#画像を二値化(白黒の画像に変換する様なもの).こっちの方が反射光の影響とか受けな胃から良さげ
+        frame = cv2.medianBlur(frame,5)
+        #print(ret)
         frame = frame[self.trim_y:self.trim_y+self.trim_h,]#画像を必要な大きさに切り取り
         LB1 = frame[self.LB_y1:self.LB_y2,self.LB_x1:self.LB_x2]#十字認識エリアの指定
         RB1 = frame[self.RB_y1:self.RB_y2,self.RB_x1:self.RB_x2]
@@ -48,31 +59,24 @@ class LineTrace:
             if m['m00'] > 0:
                 cx = int(m['m10']/m['m00'])#重心のx座標
                 cy = int(m['m01']/m['m00'])#重心のy座標
-                if Det_RB > 0 and Det_LB:#十字の認識
+                if Det_RB > 0 and Det_LB > 0:#十字の認識
                     Slip = "Cross"
-                    cx = self.Goal
+                    cx = self.cx_old + 10
                 cv2.circle(frame,(cx,cy),5,(0,255,0),-1)#重心の描画
-                err = cx - w//2#偏差を取得
+                err = cx - w//2
                 self.L_PWM_old = self.L_PWM
                 self.R_PWM_old = self.R_PWM
                 self.L_e2 = self.L_e1
                 self.R_e2 = self.R_e1
                 self.L_e1 = self.L_e
                 self.R_e1 = self.R_e
-                self.L_e = self.Goal - err
+                self.L_e = self.Goal - err#偏差を取得
                 self.R_e = self.Goal - err
                 #各要素を計算しそれらに各ゲインとの乗算を行い制御量を算出
                 self.L_PWM = self.L_PWM_old + self.Kp * (self.L_e-self.L_e1) + self.Ki * self.L_e + self.Kd * ((self.L_e-self.L_e1) - (self.L_e1-self.L_e2))
                 self.R_PWM = self.R_PWM_old + self.Kp * (self.R_e-self.R_e1) + self.Ki * self.R_e + self.Kd * ((self.R_e-self.R_e1) - (self.R_e1-self.R_e2))
+                self.cx_old = cx
         L = round(100 + self.L_PWM)#PWMの最大値から制御量を差し引きモーターに送るPWMの値を算出
         R = round(100 - self.R_PWM)
-        if L > 100:
-            L = 100
-        if L < -100:
-            L = -100
-        if R > 100:
-            R = 100
-        if R < -100:
-            R = -100
-        cv2.imshow("trace",frame)
+        #cv2.imshow("trace",frame)
         return L,R,Slip#左PWM,右PWM,十字を認識したかどうか(default:"None",十字認識時:"Cross"が格納)
